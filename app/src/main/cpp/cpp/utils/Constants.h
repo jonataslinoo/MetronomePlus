97c643107f29712f053e47d20b9c879f2859271b
jonataslinoo <jonatas.linoo@gmail.com>
2025-01-29 17:09:26 -0300

    refactor: renamed Tone Class to Beat and all Tone variables to Beat

--- app/src/main/cpp/cpp/utils/Constants.h
+++ app/src/main/cpp/cpp/utils/Constants.h
@@ -11,10 +11,10 @@ struct AudioProperties {
 constexpr int32_t kSampleRate = 48000;
 constexpr int kChannelCount = 2;
 
-//Tones
-constexpr char kNormalTone[] { "" } ;
-constexpr char kSilenceTone[] { } ;
-constexpr char kAccentTone[] { "" } ;
-constexpr char kMediumTone[] { "" } ;
+//Beats
+constexpr char kNormalBeat[] {"a_m_wood_light_tip.wav" } ;
+constexpr char kSilenceBeat[] { } ;
+constexpr char kAccentBeat[] {"a_m_wet_wood_vinyl_hit_to.wav" } ;
+constexpr char kMediumBeat[] {"a_m_wood_hit_pi.wav" } ;
 
 #endif //METRONOMEPLUS_CONSTANTS_H
