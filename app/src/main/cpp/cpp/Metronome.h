d01fcaeb99ac1fded63e1591d7858718b50389f4
jonataslinoo <jonatas.linoo@gmail.com>
2025-01-29 17:09:26 -0300

    refactor: renamed Tone Class to Beat and all Tone variables to Beat

--- app/src/main/cpp/cpp/Metronome.h
+++ app/src/main/cpp/cpp/Metronome.h
@@ -6,7 +6,7 @@
 #include <android/asset_manager.h>
 #include <oboe/Oboe.h>
 
-#include "model/Tone.h"
+#include "model/Beat.h"
 #include "audio/Player.h"
 #include "audio/Mixer.h"
 
@@ -22,29 +22,29 @@ public:
     void init();
     void end();
     void setBPM(int bpm);
-    void setTones(const std::vector<Tone> &notes);
+    void setBeats(const std::vector<Beat> &beats);
     void startPlaying();
     void stopPlaying();
 
 private:
     Mixer mMixer;
     std::shared_ptr<AudioStream> mAudioStream;
-    std::unique_ptr<Player> mNormalTonePlayer;
-    std::unique_ptr<Player> mAccentTonePlayer;
-    std::unique_ptr<Player> mMediumTonePlayer;
+    std::unique_ptr<Player> mNormalBeatPlayer;
+    std::unique_ptr<Player> mAccentBeatPlayer;
+    std::unique_ptr<Player> mMediumBeatPlayer;
 
-    std::thread mClapThread;                       // Thread para tocar as palmas
-    std::atomic<bool> mIsMetronomePlaying{};         // Controle se as palmas estão tocando
-    int mBPM{60};                                  // BPM padrão
-    int mIntervalMs{1000};                         // Intervalo entre os toques em milissegundos
+    std::thread mClapThread;                             // Thread para tocar as palmas
+    std::vector<Beat> mBeats;                            // Notas do compasso
+    std::atomic<bool> mIsMetronomePlaying{false};     // Controle se as palmas estão tocando
+    int mBPM{60};                                        // BPM padrão
+    int mIntervalMs{1000};                               // Intervalo entre os toques em milissegundos
+    int mCurrentBeatIndex{0};                            // Índice da nota atual
 
-    std::vector<Tone> mNotes;                      // Notas do compasso
-    int mCurrentNoteIndex{0};                      // Índice da nota atual
-    std::atomic<bool> mIsPlayingNotes{false};   // Controle se as notas estão sendo tocadas
+//    std::atomic<bool> mIsPlayingNotes{false};   // Controle se as notas estão sendo tocadas
 
     bool openStream();
     bool setupAudioSources();
-    bool setupPlayerTone(const char tone[], std::unique_ptr<Player> *playerTone);
+    bool setupPlayerBeat(const char beat[], std::unique_ptr<Player> *playerBeat);
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-private-field"
