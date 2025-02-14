18de7bcd503df3357545f91fbb2e77fe9d3f3020
jonataslinoo <jonatas.linoo@gmail.com>
2025-02-14 17:04:04 -0300

    feat: Add the base fragment to replace the main activity.

---
+++ app/src/main/java/br/com/jonatas/metronomeplus/presenter/ui/fragment/MetronomeFragment.kt
@@ -0,0 +1,28 @@
+package br.com.jonatas.metronomeplus.presenter.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.jonatas.metronomeplus.databinding.FragmentMetronomeBinding
+
+class MetronomeFragment : Fragment() {
+
+    private var _binding: FragmentMetronomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMetronomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
