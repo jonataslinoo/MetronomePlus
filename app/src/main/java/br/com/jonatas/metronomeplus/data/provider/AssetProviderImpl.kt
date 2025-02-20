97ff3cf57835d7c269e99efa2b516496fa152288
jonataslinoo <jonatas.linoo@gmail.com>
2025-02-20 18:30:34 -0300

    refactor: move domain interfaces to domain layer

--- app/src/main/java/br/com/jonatas/metronomeplus/data/provider/AssetProviderImpl.kt
+++ app/src/main/java/br/com/jonatas/metronomeplus/data/provider/AssetProviderImpl.kt
@@ -2,6 +2,7 @@ package br.com.jonatas.metronomeplus.data.provider
 
 import android.content.Context
 import android.content.res.AssetManager
+import br.com.jonatas.metronomeplus.domain.provider.AssetProvider
 
 class AssetProviderImpl(private val context: Context) : AssetProvider {
     override fun getAssets(): AssetManager = context.assets
