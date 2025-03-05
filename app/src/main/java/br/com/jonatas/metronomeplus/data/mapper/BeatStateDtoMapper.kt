bcef4301683bdeeea5c01957e52ffe1cf3eb8501
jonataslinoo <jonatas.linoo@gmail.com>
2025-03-05 16:24:49 -0300

    refactor(mappers): Adjust the mapping classes to make them concise and coherent, with the necessary conversions

--- app/src/main/java/br/com/jonatas/metronomeplus/data/mapper/BeatStateDtoMapper.kt
+++ app/src/main/java/br/com/jonatas/metronomeplus/data/mapper/BeatStateDtoMapper.kt
@@ -2,7 +2,6 @@ package br.com.jonatas.metronomeplus.data.mapper
 
 import br.com.jonatas.metronomeplus.data.model.BeatStateDto
 import br.com.jonatas.metronomeplus.domain.model.BeatState
-import br.com.jonatas.metronomeplus.presenter.model.BeatStateUiModel
 
 fun BeatStateDto.toDomain() = when (this) {
     BeatStateDto.Normal -> BeatState.Normal
@@ -11,9 +10,9 @@ fun BeatStateDto.toDomain() = when (this) {
     BeatStateDto.Medium -> BeatState.Medium
 }
 
-fun BeatState.toUiModel() = when (this) {
-    BeatState.Normal -> BeatStateUiModel.Normal
-    BeatState.Silence -> BeatStateUiModel.Silence
-    BeatState.Accent -> BeatStateUiModel.Accent
-    BeatState.Medium -> BeatStateUiModel.Medium
+fun BeatState.toDto() = when (this) {
+    BeatState.Normal -> BeatStateDto.Normal
+    BeatState.Silence -> BeatStateDto.Silence
+    BeatState.Accent -> BeatStateDto.Accent
+    BeatState.Medium -> BeatStateDto.Medium
 }
