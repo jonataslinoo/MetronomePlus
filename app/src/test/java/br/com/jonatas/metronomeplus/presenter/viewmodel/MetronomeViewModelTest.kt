81a5048dafa697d14a6e7d2e5bda7be908b52588
jonataslinoo <jonatas.linoo@gmail.com>
2025-03-06 15:57:41 -0300

    refactor (MetronomeViewModel): Update ViewModel to use removeBeatUseCase and adjusts corresponding tests

--- app/src/test/java/br/com/jonatas/metronomeplus/presenter/viewmodel/MetronomeViewModelTest.kt
+++ app/src/test/java/br/com/jonatas/metronomeplus/presenter/viewmodel/MetronomeViewModelTest.kt
@@ -2,7 +2,6 @@ package br.com.jonatas.metronomeplus.presenter.viewmodel
 
 import br.com.jonatas.metronomeplus.data.mapper.toDto
 import br.com.jonatas.metronomeplus.data.mapper.toDtoArray
-import br.com.jonatas.metronomeplus.data.model.BeatDto
 import br.com.jonatas.metronomeplus.data.model.MeasureDto
 import br.com.jonatas.metronomeplus.domain.engine.MetronomeEngine
 import br.com.jonatas.metronomeplus.domain.model.Beat
@@ -12,8 +11,9 @@ import br.com.jonatas.metronomeplus.domain.usecase.AddBeatUseCase
 import br.com.jonatas.metronomeplus.domain.usecase.DecreaseBpmUseCase
 import br.com.jonatas.metronomeplus.domain.usecase.GetMeasureUseCase
 import br.com.jonatas.metronomeplus.domain.usecase.IncreaseBpmUseCase
-import br.com.jonatas.metronomeplus.presenter.mapper.toDomain
+import br.com.jonatas.metronomeplus.domain.usecase.RemoveBeatUseCase
 import br.com.jonatas.metronomeplus.presenter.mapper.toUiModel
+import br.com.jonatas.metronomeplus.presenter.mapper.toUiModelList
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
@@ -27,7 +27,6 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
-import org.mockito.ArgumentMatchers.anyList
 import org.mockito.Mock
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.never
@@ -53,6 +52,10 @@ class MetronomeViewModelTest {
     @Mock
     private lateinit var mockAddBeatUseCase: AddBeatUseCase
 
+    @Mock
+    private lateinit var mockRemoveBeatUseCase: RemoveBeatUseCase
+    private val testDispatcher = StandardTestDispatcher()
+
     private val viewModel by lazy {
         MetronomeViewModel(
             mockMetronomeEngine,
@@ -60,10 +63,10 @@ class MetronomeViewModelTest {
             mockIncreaseBpmUseCase,
             mockDecreaseBpmUseCase,
             mockAddBeatUseCase,
+            mockRemoveBeatUseCase,
             testDispatcher
         )
     }
-    private val testDispatcher = StandardTestDispatcher()
 
     @Before
     fun setup() {
@@ -230,9 +233,7 @@ class MetronomeViewModelTest {
         runTest(testDispatcher) {
             val initialMeasure = Measure(
                 bpm = 120,
-                beats = listOf(
-                    Beat(BeatState.Accent),
-                )
+                beats = listOf(Beat(BeatState.Accent))
             )
             val expectedMeasure =
                 initialMeasure.copy(beats = initialMeasure.beats + Beat(BeatState.Normal))
@@ -258,7 +259,7 @@ class MetronomeViewModelTest {
         }
 
     @Test
-    fun `should remove last index from the beat list when removeBeat is called in the viewModel`() =
+    fun `should remove last index from the beat list when removeBeat is called`() =
         runTest(testDispatcher) {
             val initialMeasure = Measure(
                 bpm = 120,
@@ -266,55 +267,31 @@ class MetronomeViewModelTest {
                     Beat(BeatState.Accent),
                     Beat(BeatState.Normal),
                     Beat(BeatState.Normal),
-                    Beat(BeatState.Normal),
+                    Beat(BeatState.Normal)
                 )
             )
-            val expectedBeats = initialMeasure.toUiModel().beats.toMutableList()
+            val expectedBeats =
+                initialMeasure.beats.toMutableList().apply {
+                    removeAt(initialMeasure.beats.lastIndex)
+                }
             `when`(mockGetMeasureUseCase()).thenReturn(initialMeasure)
+            `when`(mockRemoveBeatUseCase(initialMeasure.beats)).thenReturn(expectedBeats)
 
             viewModel.removeBeat()
             advanceUntilIdle()
 
             val stateReady = viewModel.uiState.first()
-            expectedBeats.removeAt(initialMeasure.beats.lastIndex)
-
+            assertTrue(stateReady is MetronomeViewModel.MetronomeState.Ready)
             assertEquals(
-                expectedBeats,
+                expectedBeats.toUiModelList(),
                 (stateReady as MetronomeViewModel.MetronomeState.Ready).measure.beats
             )
             assertEquals(
-                3,
+                expectedBeats.size,
                 (stateReady as MetronomeViewModel.MetronomeState.Ready).measure.beats.size
             )
 
-            val domainBeats = expectedBeats.map { it.toDomain() }
-            verify(mockMetronomeEngine).setBeats(domainBeats.toDtoArray())
-        }
-
-    @Test
-    fun `should not remove the beat if there is only one beat when removeBeat is called int the viewModel `() =
-        runTest(testDispatcher) {
-            val initialMeasure = Measure(
-                bpm = 120, beats = listOf(Beat(BeatState.Accent))
-            )
-            val expectedBeats = initialMeasure.toUiModel().beats
-            `when`(mockGetMeasureUseCase()).thenReturn(initialMeasure)
-
-            viewModel.removeBeat()
-            advanceUntilIdle()
-
-            val stateReady = viewModel.uiState.first()
-
-            assertEquals(
-                expectedBeats,
-                (stateReady as MetronomeViewModel.MetronomeState.Ready).measure.beats
-            )
-            assertEquals(
-                1,
-                (stateReady as MetronomeViewModel.MetronomeState.Ready).measure.beats.size
-            )
-
-            val domainBeats = expectedBeats.map { it.toDomain() }
-            verify(mockMetronomeEngine, never()).setBeats(domainBeats.toDtoArray())
+            verify(mockRemoveBeatUseCase).invoke(initialMeasure.beats)
+            verify(mockMetronomeEngine).setBeats(expectedBeats.toDtoArray())
         }
 }
