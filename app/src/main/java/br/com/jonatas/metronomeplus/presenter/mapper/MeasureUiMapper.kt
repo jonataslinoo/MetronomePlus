d1479875336d7d9b90b9b045ea66b9199a0a4771
jonataslinoo <jonatas.linoo@gmail.com>
2025-03-06 15:30:57 -0300

    fix (MeasureUiMapper): Adjust the state of isPlaying when adding a new beat to the beat list. - Add an extension function in the user interface mapping to convert beat lists into beatUiModel with toUiModelList.

--- app/src/main/java/br/com/jonatas/metronomeplus/presenter/mapper/MeasureUiMapper.kt
+++ app/src/main/java/br/com/jonatas/metronomeplus/presenter/mapper/MeasureUiMapper.kt
@@ -1,14 +1,18 @@
 package br.com.jonatas.metronomeplus.presenter.mapper
 
+import br.com.jonatas.metronomeplus.domain.model.Beat
 import br.com.jonatas.metronomeplus.domain.model.Measure
+import br.com.jonatas.metronomeplus.presenter.model.BeatUiModel
 import br.com.jonatas.metronomeplus.presenter.model.MeasureUiModel
 
 /** Domain for UI */
 fun Measure.toUiModel(): MeasureUiModel = MeasureUiModel(
     isPlaying = false,
     bpm = bpm,
-    beats = beats.map { it.toUiModel() }.toList()
+    beats = beats.toUiModelList()
 )
+
+fun List<Beat>.toUiModelList(): List<BeatUiModel> = map { it.toUiModel() }
 
 /** Ui for Domain */
 fun MeasureUiModel.toDomain(): Measure = Measure(
