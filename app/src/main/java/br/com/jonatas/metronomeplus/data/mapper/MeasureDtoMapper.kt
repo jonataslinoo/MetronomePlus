b5637e1396c19034ae88ab7fc8002e7a695a3f1b
jonataslinoo <jonatas.linoo@gmail.com>
2025-03-07 17:55:03 -0300

    refactor (metronomeViewModelTest): Adjust the tests to use list conversions instead of Measure directly.

--- app/src/main/java/br/com/jonatas/metronomeplus/data/mapper/MeasureDtoMapper.kt
+++ app/src/main/java/br/com/jonatas/metronomeplus/data/mapper/MeasureDtoMapper.kt
@@ -8,7 +8,7 @@ import br.com.jonatas.metronomeplus.domain.model.Measure
 /** Domain for Dto */
 fun Measure.toDto(): MeasureDto = MeasureDto(
     bpm = bpm,
-    beats = beats.map { it.toDto() }.toList()
+    beats = beats.map { it.toDto() }
 )
 
 fun List<Beat>.toDtoArray(): Array<BeatDto> = map { it.toDto() }.toTypedArray()
@@ -16,5 +16,5 @@ fun List<Beat>.toDtoArray(): Array<BeatDto> = map { it.toDto() }.toTypedArray()
 /** Dto for Domain */
 fun MeasureDto.toDomain(): Measure = Measure(
     bpm = bpm,
-    beats = beats.map { it.toDomain() }.toList()
+    beats = beats.map { it.toDomain() }
 )
