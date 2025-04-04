32f3e3233ff376e5527110e3c3c75e2921d910c1
jonataslinoo <jonatas.linoo@gmail.com>
2025-04-04 19:07:59 -0300

    feat (MainActivity): Add a lower navigation bar to the main activity, to navigate between fragments and create a navigation graph - Add tests to verify correct navigation between fragments - Add new styles and strings for using the bottom navigation bar

--- app/src/main/java/br/com/jonatas/metronomeplus/presenter/ui/MainActivity.kt
+++ app/src/main/java/br/com/jonatas/metronomeplus/presenter/ui/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import br.com.jonatas.metronomeplus.R
 import br.com.jonatas.metronomeplus.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : BaseActivity() {
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +24,20 @@ class MainActivity : BaseActivity() {
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupNavController()
+    }
+
+    private fun setupNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val navView: BottomNavigationView = binding.bottomNavigationView
+        navView.itemIconTintList = null
+        navView.setupWithNavController(navController)
     }
 }
