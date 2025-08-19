cf0d2a9e87a2cb522803b315ca15b74e3774a51b
jonataslinoo <jonatas.linoo@gmail.com>
2025-08-18 21:38:42 -0300

    refactor (MyApplication): Adjust, add annotations, and refactor code for use with Hilt.

--- app/src/main/java/br/com/jonatas/metronomeplus/presenter/MyApplication.kt
+++ app/src/main/java/br/com/jonatas/metronomeplus/presenter/MyApplication.kt
@@ -1,22 +1,7 @@
 package br.com.jonatas.metronomeplus.presenter
 
 import android.app.Application
-import br.com.jonatas.metronomeplus.data.local.DataStoreManager
+import dagger.hilt.android.HiltAndroidApp
 
-class MyApplication : Application() {
-
-    lateinit var dataStoreManager: DataStoreManager
-        private set
-
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
-
-        dataStoreManager = DataStoreManager.getInstance(context = this)
-    }
-
-    companion object {
-        lateinit var instance: MyApplication
-            private set
-    }
-}
+@HiltAndroidApp
+class MyApplication : Application()
